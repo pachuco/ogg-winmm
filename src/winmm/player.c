b4aa3347efa6a6359d2500bf23b5fe5383efdd20
pachuco <ifail@life.net>
2019-03-08 14:25:56 +0200

    Apply favorite indenting.

--- src/winmm/player.c
+++ src/winmm/player.c
@@ -1,6 +1,7 @@
 #include <vorbis/vorbisfile.h>
 #include <stdio.h>
 #include <windows.h>
+#include <stdint.h>
 
 WAVEFORMATEX    plr_fmt;
 HWAVEOUT        plr_hwo         = NULL;
@@ -10,28 +11,22 @@ int             plr_cnt         = 0;
 int             plr_vol         = 100;
 WAVEHDR         *plr_buffers[3] = { NULL, NULL, NULL };
 
-void plr_stop()
-{
+void plr_stop() {
     plr_cnt = 0;
 
-    if (plr_vf.datasource)
-        ov_clear(&plr_vf);
+    if (plr_vf.datasource) ov_clear(&plr_vf);
 
-    if (plr_ev)
-    {
+    if (plr_ev) {
         CloseHandle(plr_ev);
         plr_ev = NULL;
     }
 
-    if (plr_hwo)
-    {
+    if (plr_hwo) {
         waveOutReset(plr_hwo);
 
         int i;
-        for (i = 0; i < 3; i++)
-        {
-            if (plr_buffers[i] && plr_buffers[i]->dwFlags & WHDR_DONE)
-            {
+        for (i = 0; i < 3; i++) {
+            if (plr_buffers[i] && plr_buffers[i]->dwFlags & WHDR_DONE) {
                 waveOutUnprepareHeader(plr_hwo, plr_buffers[i], sizeof(WAVEHDR));
                 free(plr_buffers[i]->lpData);
                 free(plr_buffers[i]);
@@ -44,15 +39,13 @@ void plr_stop()
     }
 }
 
-void plr_volume(int vol)
-{
+void plr_volume(int vol) {
     if (vol < 0) vol = 0;
     if (vol > 100) vol = 100;
     plr_vol = vol;
 }
 
-int plr_length(const char *path)
-{
+int plr_length(const char *path) {
     OggVorbis_File  vf;
 
     if (ov_fopen(path, &vf) != 0)
@@ -65,17 +58,14 @@ int plr_length(const char *path)
     return ret;
 }
 
-int plr_play(const char *path)
-{
+int plr_play(const char *path) {
     plr_stop();
 
-    if (ov_fopen(path, &plr_vf) != 0)
-        return 0;
+    if (ov_fopen(path, &plr_vf) != 0) return 0;
 
     vorbis_info *vi = ov_info(&plr_vf, -1);
 
-    if (!vi)
-    {
+    if (!vi) {
         ov_clear(&plr_vf);
         return 0;
     }
@@ -90,63 +80,52 @@ int plr_play(const char *path)
 
     plr_ev = CreateEvent(NULL, 0, 1, NULL);
 
-    if (waveOutOpen(&plr_hwo, WAVE_MAPPER, &plr_fmt, (DWORD_PTR)plr_ev, 0, CALLBACK_EVENT) != MMSYSERR_NOERROR)
-    {
+    if (waveOutOpen(&plr_hwo, WAVE_MAPPER, &plr_fmt, (DWORD_PTR)plr_ev, 0, CALLBACK_EVENT) != MMSYSERR_NOERROR) {
         return 0;
     }
 
     return 1;
 }
 
-int plr_pump()
-{
-    if (!plr_vf.datasource)
-        return 0;
+int plr_pump() {
+    if (!plr_vf.datasource) return 0;
 
     int pos = 0;
     int bufsize = plr_fmt.nAvgBytesPerSec / 4; // 250ms (avg at 500ms) should be enough for everyone
     char *buf = malloc(bufsize);
 
-    while (pos < bufsize)
-    {
+    while (pos < bufsize) {
         long bytes = ov_read(&plr_vf, buf + pos, bufsize - pos, 0, 2, 1, NULL);
 
-        if (bytes == OV_HOLE)
-        {
+        if (bytes == OV_HOLE) {
             free(buf);
             continue;
         }
 
-        if (bytes == OV_EBADLINK)
-        {
+        if (bytes == OV_EBADLINK) {
             free(buf);
             return 0;
         }
 
-        if (bytes == OV_EINVAL)
-        {
+        if (bytes == OV_EINVAL) {
             free(buf);
             free(buf);
             return 0;
         }
 
-        if (bytes == 0)
-        {
+        if (bytes == 0) {
             free(buf);
 
             int i, in_queue = 0;
-            for (i = 0; i < 3; i++)
-            {
-                if (plr_buffers[i] && plr_buffers[i]->dwFlags & WHDR_DONE)
-                {
+            for (i = 0; i < 3; i++) {
+                if (plr_buffers[i] && plr_buffers[i]->dwFlags & WHDR_DONE) {
                     waveOutUnprepareHeader(plr_hwo, plr_buffers[i], sizeof(WAVEHDR));
                     free(plr_buffers[i]->lpData);
                     free(plr_buffers[i]);
                     plr_buffers[i] = NULL;
                 }
 
-                if (plr_buffers[i])
-                    in_queue++;
+                if (plr_buffers[i]) in_queue++;
             }
 
             Sleep(100);
@@ -160,8 +139,9 @@ int plr_pump()
     // volume control, kinda nasty
     int x, end = pos / 2;
     short *sbuf = (short *)buf;
-    for (x = 0; x < end; x++)
+    for (x = 0; x < end; x++) {
         sbuf[x] = sbuf[x] * (plr_vol / 100.0f);
+    }
         
 
     WAVEHDR *header = malloc(sizeof(WAVEHDR));
@@ -175,32 +155,27 @@ int plr_pump()
 
     waveOutPrepareHeader(plr_hwo, header, sizeof(WAVEHDR));
 
-    if (plr_cnt > 1)
-    {
+    if (plr_cnt > 1) {
         WaitForSingleObject(plr_ev, INFINITE);
     }
 
     int i, queued = 0;
-    for (i = 0; i < 3; i++)
-    {
-        if (plr_buffers[i] && plr_buffers[i]->dwFlags & WHDR_DONE)
-        {
+    for (i = 0; i < 3; i++) {
+        if (plr_buffers[i] && plr_buffers[i]->dwFlags & WHDR_DONE) {
             waveOutUnprepareHeader(plr_hwo, plr_buffers[i], sizeof(WAVEHDR));
             free(plr_buffers[i]->lpData);
             free(plr_buffers[i]);
             plr_buffers[i] = NULL;
         }
 
-        if (!queued && plr_buffers[i] == NULL)
-        {
+        if (!queued && plr_buffers[i] == NULL) {
             waveOutWrite(plr_hwo, header, sizeof(WAVEHDR));
             plr_buffers[i] = header;
             queued = 1;
         }
     }
 
-    if (!queued)
-    {
+    if (!queued) {
         free(header);
         free(buf);
     }
