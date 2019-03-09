b33e2c449ffabfa279f529479948806cca225d88
pachuco <ifail@life.net>
2019-03-09 20:31:31 +0200

    Added MCI testing application of sorts.

--- build.bat
+++ build.bat
@@ -13,17 +13,18 @@ for %%a in (%*) DO (
 )
 
 if [%gccbase%] neq [] set PATH=%PATH%;%gccbase%
-
 set bin=%CD%\bin
 set tmp=%CD%\tmp
 set err=%CD%
+
 set l_ogg=libogg-1.3.3
 set l_vorb=libvorbis-1.3.6
 set l_winmm=winmm
-set l_rc=wav-winmm
+set l_testmci=testmci
 
 call :del err_*.log 0
 call :del %bin%\%l_winmm%.dll 0
+call :del %bin%\%l_testmci%.exe 0
 
 set name=%l_ogg%
 set opts=-m32 -std=gnu99 -O2 -shared -s
@@ -31,8 +32,9 @@ set files=framing.c bitwise.c
 set incl=-I.
 call :compile_ar %name% "%files% %opts% %incl% %links%"
 
+
 set name=%l_vorb%
-set opts=-m32 -std=gnu99 -O2 -s -static-libgcc
+set opts=-m32 -std=gnu99 -O2 -s
 ::psytune.c tone.c barkmel.c
 set files=analysis.c bitrate.c block.c codebook.c envelope.c floor0.c floor1.c info.c lookup.c lpc.c lsp.c mapping0.c mdct.c psy.c registry.c res0.c sharedbook.c smallft.c synthesis.c vorbisenc.c vorbisfile.c window.c
 set files=%files% %bin%\%l_ogg%.a
@@ -40,17 +42,22 @@ set incl=-I. -I..\%l_ogg%
 set links=
 call :compile_ar %name% "%files% %opts% %incl% %links%"
 
-%WINDRES% src\%l_winmm%\%l_rc%.rc.in -O coff -o %bin%\%l_rc%.o
-
+%WINDRES% src\%l_winmm%\wav-winmm.rc -O coff -o %bin%\wav-winmm.o
 set name=%l_winmm%
-set opts=-m32 -std=gnu99 -Wl,--enable-stdcall-fixup -O2 -shared -s
+set opts=wav-winmm.def -m32 -std=gnu99 -Wl,--enable-stdcall-fixup -O2 -shared -s
 if defined M_DEBUG set opts=%opts% -D _DEBUG
-set files=player.c wav-winmm.c stubs.c wav-winmm.def
-set files=%files% %bin%\%l_vorb%.a %bin%\%l_ogg%.a %bin%\%l_rc%.o
+set files=player.c wav-winmm.c stubs.c cdplayer.c winmm_out.c
+set files=%files% %bin%\%l_vorb%.a %bin%\%l_ogg%.a %bin%\wav-winmm.o
 set incl=-I. -I..\%l_vorb% -I..\%l_ogg%
-set links=-lwinmm -lkernel32
-call :compile_dll %name% "%files% %opts% %incl% %links%"
-set includes=-I
+set links=-lwinmm
+call :compile_bin %name% "%files% %opts% %incl% %links%" dll
+
+set name=%l_testmci%
+set opts=-mconsole -m32 -std=gnu99 -Wl,--enable-stdcall-fixup -O2 -s
+set files=testmci.c
+set incl=-I. -I..\%l_vorb% -I..\%l_ogg%
+set links=-lwinmm
+call :compile_bin %name% "%files% %opts% %incl% %links%" exe
 
 echo .
 echo all done!
@@ -59,16 +66,14 @@ exit /B 0
 
 
 
-
-
-:compile_dll
-::objname params
-if not defined M_FORCE ( if exist %bin%\%~1.dll (
-    echo skipped %~1.dll
+:compile_bin
+::objname params extension
+if not defined M_FORCE ( if exist %bin%\%~1.%~3 (
+    echo skipped %~1.%~3
     exit /B 0
 ))
 pushd src\%~1
-%GCC% -o %bin%\%~1.dll %~2 %p% 2> %err%\err_%~1.log
+%GCC% -o %bin%\%~1.%~3 %~2 %p% 2> %err%\err_%~1.log
 call :errcheck %~1
 call :del %err%\err_%~1.log 1
 call :del *.o 0
@@ -102,6 +107,7 @@ if %errorlevel% neq 0 (
     echo oops %~1!
     pause
     call :del %err%\err_%~1.log 1
+    call :del ..\%~1\*.o 0
     call :del %tmp%\* 0
     exit
 )
---
+++ src/testmci/testmci.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <windows.h>
+
+#define BUFSIZE 1024
+static char comBuf[BUFSIZE];
+static char errBuf[BUFSIZE];
+static char retBuf[BUFSIZE];
+
+static size_t _getline(char* lineptr, size_t n, FILE* stream) {
+    int i;
+
+    if(!lineptr) return 0;
+    fgets(lineptr, n, stream);
+    for(i=0; i<n; i++) {
+        if (lineptr[i]=='\n') lineptr[i] = '\0';
+        if (lineptr[i]=='\0') break;
+    }
+    
+    return i;
+}
+
+int main(int argc, char* argv[]) {
+    size_t size;
+    MCIERROR err;
+    
+    printf("Welcome to the MCI commandline.\n");
+    printf("Type at your heart's content.\n");
+    printf("Type \"exit\" to close MCI commandline.\n\n");
+    printf("Type \"exit\" to close MCI commandline.\n\n");
+    while(TRUE) {
+        printf("MCI>");
+        size = _getline(comBuf, BUFSIZE, stdin);
+        err = mciSendStringA(comBuf, retBuf, BUFSIZE, NULL);
+        mciGetErrorString(err, errBuf, BUFSIZE);
+        printf("ret|%s\n", retBuf);
+        printf("err|%s\n", errBuf);
+    }
+    
+    return 0;
+}
